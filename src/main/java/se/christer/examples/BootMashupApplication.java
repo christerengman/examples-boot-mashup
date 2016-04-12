a2a94b5647dd173a731159d71b3f612555a28ef5
Christer Engman <christer@christer.se>
2016-04-12 22:32:03 +0200

    Spring Initializr

---
+++ src/main/java/se/christer/examples/BootMashupApplication.java
@@ -0,0 +1,12 @@
+package se.christer.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMashupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMashupApplication.class, args);
+	}
+}
